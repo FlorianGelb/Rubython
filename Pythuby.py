be436c23adea5fc1b1b41bb1fd3d91fbb6896065
Florian Gelb <irolori64@gmail.com>
2018-10-21 16:53:47 +0200

    Complete Pythuby.py

--- Pythuby.py
+++ Pythuby.py
@@ -6,7 +6,7 @@ class Pythuby:
     def __init__(self, code = False, file = False):
         if code:
             self.code = code
-
+            self.pythuby()
         if file:
             self.file = file
 
@@ -14,26 +14,15 @@ class Pythuby:
         with open("Temp.rb", "w") as temp_rb_script:
             temp_rb_script.write(self.code)
             temp_rb_script.close()
-            self.runPythuby()
+
 
     def runPythuby(self):
         cmd = subprocess.Popen("ruby Temp.rb", shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE,stderr=subprocess.PIPE)
-        result = cmd.stdout.read()
+        result = cmd.stdout.read().decode("UTF-8")
         #os.remove("Temp.rb")
-        #return (result)
-
-    def inculde(self):
-        cmd = subprocess.Popen("ruby {}".format(self.file), shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        result = cmd.stdout.read() + cmd.stderr.read()
         return (result)
 
-
-
-code = "a = [1,2,3,4,5,6,7]\n" \
-       "a.each do |e|\n" \
-       "puts e.odd?\n" \
-       "end"
-
-a = Pythuby(code = code, file=False)
-print(a.pythuby())
-print(a)
+    def include(self, file):
+        cmd = subprocess.Popen("ruby {}".format(file), shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        result = (cmd.stdout.read() + cmd.stderr.read()).decode("UTF-8")
+        return (result)
